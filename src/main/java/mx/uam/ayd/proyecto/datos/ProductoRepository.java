ecee40e4eaba7c385aa68723cc6bb23ce7eb9b30
Braulio Sánchez <02brausanchezz@gmail.com>
2025-07-29 12:05:01 -0600

    Implementación de ProductolRepository basado en CrudRepository

---
+++ src/main/java/mx/uam/ayd/proyecto/datos/ProductoRepository.java
@@ -0,0 +1,9 @@
+package mx.uam.ayd.proyecto.datos;
+
+import java.util.List;
+
+import org.springframework.data.repository.CrudRepository;
+
+import mx.uam.ayd.proyecto.negocio.modelo.Usuario;
+
+public interface ProductoRepository extends CrudRepository <Usuario, Long> { }

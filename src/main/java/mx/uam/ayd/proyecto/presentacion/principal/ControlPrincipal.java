cae0293f0f5dcdcbe00818a5240d86101cec5c84
Braulio Sánchez <02brausanchezz@gmail.com>
2025-08-06 12:11:10 -0600

    [ControlPrincipal] Se agregó el atributo controlConfiguracionUmbrales y el método configurarUmbrales() para iniciar la historia de usuario "configurar umbrales"

--- src/main/java/mx/uam/ayd/proyecto/presentacion/principal/ControlPrincipal.java
+++ src/main/java/mx/uam/ayd/proyecto/presentacion/principal/ControlPrincipal.java
@@ -8,6 +8,7 @@ import org.springframework.stereotype.Component;
 import mx.uam.ayd.proyecto.presentacion.agregarUsuario.ControlAgregarUsuario;
 import mx.uam.ayd.proyecto.presentacion.listarUsuarios.ControlListarUsuarios;
 import mx.uam.ayd.proyecto.presentacion.listarGrupos.ControlListarGrupos;
+import mx.uam.ayd.proyecto.presentacion.configurarUmbrales.ControlConfiguracionUmbrales;
 
 /**
  * Esta clase lleva el flujo de control de la ventana principal
@@ -21,17 +22,21 @@ public class ControlPrincipal {
 	private final ControlAgregarUsuario controlAgregarUsuario;
 	private final ControlListarUsuarios controlListarUsuarios;
 	private final ControlListarGrupos controlListarGrupos;
+	private final ControlConfiguracionUmbrales controlConfiguracionUmbrales;
 	private final VentanaPrincipal ventana;
+
 	
 	@Autowired
 	public ControlPrincipal(
 			ControlAgregarUsuario controlAgregarUsuario,
 			ControlListarUsuarios controlListarUsuarios,
 			ControlListarGrupos controlListarGrupos,
+			ControlConfiguracionUmbrales controlConfiguracionUmbrales,
 			VentanaPrincipal ventana) {
 		this.controlAgregarUsuario = controlAgregarUsuario;
 		this.controlListarUsuarios = controlListarUsuarios;
 		this.controlListarGrupos = controlListarGrupos;
+		this.controlConfiguracionUmbrales = controlConfiguracionUmbrales;
 		this.ventana = ventana;
 	}
 	
@@ -51,6 +56,12 @@ public class ControlPrincipal {
 	public void inicia() {
 		ventana.muestra();
 	}
+
+	/*
+		Metodo que arranca la historia de usuario "configurar umbrales"
+	 */
+	public void configurarUmbrales() {controlConfiguracionUmbrales.inicia();}
+
 
 	/**
 	 * Método que arranca la historia de usuario "agregar usuario"

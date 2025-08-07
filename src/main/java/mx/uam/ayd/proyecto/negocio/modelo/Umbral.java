bf482aadd8803ad7328c5b32bee47e606894f99b
Braulio Sánchez <02brausanchezz@gmail.com>
2025-08-07 16:13:47 -0600

    Se mantiene sin cambios necesarios.

--- src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Umbral.java
+++ src/main/java/mx/uam/ayd/proyecto/negocio/modelo/Umbral.java
@@ -1,11 +1,6 @@
 package mx.uam.ayd.proyecto.negocio.modelo;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.OneToOne;
-import jakarta.persistence.JoinColumn;
+import jakarta.persistence.*;
 
 /**
  * Entidad de negocio Umbral
@@ -16,15 +11,20 @@ import jakarta.persistence.JoinColumn;
 @Entity
 public class Umbral{
 
+    //@OneToOne(cascade = CascadeType.ALL)
+    //@JoinColumn(name = "producto_id")
+    //private Producto producto;
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long idUmbral;
 
     private int valorMinimo;
 
-    @OneToOne
-    @JoinColumn(name = "producto_id")
-    private Producto producto;
+   @OneToOne
+   @JoinColumn(name = "producto_id")
+   private Producto producto;
 
     @OneToOne
     @JoinColumn(name = "alerta_id")
